89e8fb44d54e496c85de250307732aa36e7223a8
ked ked <np_batu@hotmail.com>
2021-05-06 13:10:46 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintanceTime = "Sistem bakımda";
+        public static string ProductListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        internal static string UserRegistered = "Eklendi";
+        internal static string UserNotFound = "Kullanıcı adı hatalı";
+        internal static string PasswordError = "Yanlış password";
+        internal static string SuccessfulLogin = "Başarılı giriş";
+        internal static string UserAlreadyExists = "Bu isimde kullanıcı mevcut";
+        internal static string AccessTokenCreated = "Token oluşturuldu";
+        internal static string ProductUpdated;
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,45 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+            //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var item in categoryManager.GetAll().Data)
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal(), new CategoryManager(new EfCategoryDal()));
+
+            var result = productManager.GetProductDetails();
+
+            if (result.Success)
+            {
+                foreach (var item in result.Data)
+                {
+                    Console.WriteLine(item.ProductName + " / " + item.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }  
+        }
+    }
+}
